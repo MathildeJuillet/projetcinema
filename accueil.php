07fb19ddcfac121214500ff39f5874f4040253c4
MathildeJuillet <juilletmathilde7@gmail.com>
2017-03-01 13:11:33 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Nom du site</title>
+</head>
+<body>
+  <div class="header">
+    <div class="button" id="accueil">Accueil</div>
+    <div class="button" id="connexion"></div>
+    <div class="button" id="recommandes"></div>
+    <div class="button" id="new"></div>
+    <div class="button" id="ma_page"></div>
+    <div class="button" id="devise">On pourrait mettre une devise ici</div>
+    <div id ="rech"></div>
+    <div class="header" id="logo">LOGO</div>
+  </div>
+  <div class = "corps">
+    <div class = "corps" id="nouveautes">
+        <p>div nouveautés<p>
+    </div>
+    <div class = "corps" id="tires_au_hasard">
+        <p>div tirés au hasard<p>
+    </div>
+  </div>
+</body>
+  <div class= "footer">
+    <p>ceci est un footer</p>
+    <div class="footer" id="Facebook">
+    <p> div Facebook</p>
+  </div>
+  <div class="footer" id="Twitter">
+    <p> div Twitter</p>
+  </div>
+  <div class="footer" id="mentions_legales">
+    <p> div mentions légales</p>
+  </div>
+  </div>
+</html>
