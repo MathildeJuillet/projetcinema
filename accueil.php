df5f32db37c2d3ba4dc185fd08928e8cd03972d1
MathildeJuillet <juilletmathilde7@gmail.com>
2017-03-08 11:15:15 +0100

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -20,9 +20,68 @@
   <div class = "corps">
     <div class = "corps" id="nouveautes">
         <p>div nouveautés<p>
+          <?php
+
+          $servername = "10.0.3.100";
+          $username = "equipe";
+          $password = "coucou";
+          $dbname = "cinema";
+
+           $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if ($conn->connect_error) {
+               die("Connection failed: " . $conn->connect_error);
+            }
+          $sql = "SELECT titre, affiche FROM film";
+            $result = $conn->query($sql);
+          echo"<table border = '2'>";
+          if ($result->num_rows > 0) {
+               // output data of each row
+               echo"<tr>";
+               while($row = $result->fetch_assoc()) {
+
+                echo"<td><img src=".$row["affiche"]." width='150px'>";
+                echo"<br>".$row["titre"]."</td>";
+
+               }
+               echo "</tr>";
+            }
+            echo"</table>";
+            $conn->close();
+
+          ?>
+
     </div>
     <div class = "corps" id="tires_au_hasard">
         <p>div tirés au hasard<p>
+          <?php
+
+          $servername = "10.0.3.100";
+          $username = "equipe";
+          $password = "coucou";
+          $dbname = "cinema";
+          $nb_rand =rand(1,4);
+           $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if ($conn->connect_error) {
+               die("Connection failed: " . $conn->connect_error);
+            }
+          $sql = "SELECT idf,titre, affiche FROM film where idf=".$nb_rand;
+          $result = $conn->query($sql);
+          echo"<table border = '2'>";
+          if ($result->num_rows > 0) {
+               // output data of each row
+               echo"<tr>";
+               while($row = $result->fetch_assoc()) {
+                echo"<td><img src=".$row["affiche"]." width='150px'>";
+                echo"<br>".$row["titre"]."</td>";
+
+               }
+               echo "</tr>";
+            }
+            echo"</table>";
+              $conn->close();
+          ?>
     </div>
   </div>
   <div class= "footer">
--- rand.php
+++ rand.php
@@ -4,20 +4,19 @@ $servername = "10.0.3.100";
 $username = "equipe";
 $password = "coucou";
 $dbname = "cinema";
-
+$nb_rand =rand(1,4);
  $conn = new mysqli($servername, $username, $password, $dbname);
 
   if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
   }
-$sql = "SELECT titre, affiche FROM film";
-  $result = $conn->query($sql);
+$sql = "SELECT idf,titre, affiche FROM film where idf=".$nb_rand;
+$result = $conn->query($sql);
 echo"<table border = '2'>";
 if ($result->num_rows > 0) {
      // output data of each row
      echo"<tr>";
      while($row = $result->fetch_assoc()) {
-
       echo"<td><img src=".$row["affiche"]." width='250px'>";
       echo"<br>".$row["titre"]."</td>";
 
@@ -25,6 +24,5 @@ if ($result->num_rows > 0) {
      echo "</tr>";
   }
   echo"</table>";
-  $conn->close();
-
+    $conn->close();
 ?>
