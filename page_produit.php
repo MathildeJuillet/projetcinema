032ab3b754f765c982bd405b4c0c4c5251ddb25d
BenjaminBinard <benjii.binard@gmail.com>
2017-03-09 15:51:50 +0100

    Update page_produit.php

--- page_produit.php
+++ page_produit.php
@@ -1,20 +1,35 @@
-<!DOCTYPE html>
-
+<?php
+session_start();
+ ?>
+ <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <link rel="stylesheet" href="styleT.css" />
-  <title>Nom du site</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="styleT.css" />
+    <title>Orange Mech'anique</title>
 </head>
 <body>
-
   <div class="header">
-    <div class="button" id="accueil">Accueil</div>
-    <div class="button" id="connexion">Connexion</div>
-    <div class="button" id="recommandes">Recommandés</div>
-    <div class="button" id="new">Nouveautés</div>
-    <div class="button" id="ma_page">Ma page</div>
-    <div id="devise">On pourrait mettre une devise ici</div>
+      <a href='accueil.php'><div class="button" id="accueil">Accueil</div></a>
+      <?php
+      if (isset($_SESSION['login'])) {
+        echo "<a href='deconnexion.php'><div class='button' id='connexion'>Deconnexion</div></a>";
+      }
+      else {
+        echo "<a href='connexion.php'><div class='button' id='connexion'>Connexion</div></a>";
+      }
+      ?>
+      <a href='recommandes.php'><div class="button" id="recommandes">Recommandés</div></a>
+      <a href='noueautes.php'><div class="button" id="new">Nouveautés</div></a>
+      <?php
+      if (isset($_SESSION['login'])) {
+        echo "<a href='ma_page.php'><div class='button' id='ma_page'>Ma page</div></a>";
+      }
+      else {
+        echo "<a href='register.php'><div class='button' id='ma_page'>S'inscrire</div></a>";
+      }
+      ?>
+    <div id="devise">Orange Mech'Anique</div>
     <div id ="rech"></div>
     <div class="header" id="logo"></div>
   </div>
