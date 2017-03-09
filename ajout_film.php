22dd6eacfa4b9bffb68415cd4f0089e0eba95f78
BenjaminBinard <benjii.binard@gmail.com>
2017-03-09 15:55:51 +0100

    Add files via upload

--- ajout_film.php
+++ ajout_film.php
@@ -1,11 +1,35 @@
-<!doctype html>
+<?php
+session_start();
+ ?>
+ <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="style.css" />
-    <title>Register</title>
+    <link rel="stylesheet" href="styleT.css" />
+    <title>Orange Mech'anique</title>
 </head>
 <body>
+  <div class="header">
+      <a href='accueil.php'><div class="button" id="accueil">Accueil</div></a>
+      <?php
+      if (isset($_SESSION['login'])) {
+        echo "<a href='deconnexion.php'><div class='button' id='connexion'>Deconnexion</div></a>";
+      }
+      else {
+        echo "<a href='connexion.php'><div class='button' id='connexion'>Connexion</div></a>";
+      }
+      ?>
+      <a href='recommandes.php'><div class="button" id="recommandes">Recommandés</div></a>
+      <a href='noueautes.php'><div class="button" id="new">Nouveautés</div></a>
+      <?php
+      if (isset($_SESSION['login'])) {
+        echo "<a href='ma_page.php'><div class='button' id='ma_page'>Ma page</div></a>";
+      }
+      else {
+        echo "<a href='register.php'><div class='button' id='ma_page'>S'inscrire</div></a>";
+      }
+      ?>
+    <div id="devise">Orange Mech'Anique</div>
 	<div id="register_box">
 	<form id="ajout" method="post" enctype="multipart/form-data">
 		Titre<br>
