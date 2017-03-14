8a82d80ed6093e754e23330e42ce7c4ff76d25cc
MathildeJuillet <juilletmathilde7@gmail.com>
2017-03-14 13:52:07 +0100

    Update recherche.php

--- recherche.php
+++ recherche.php
@@ -1,37 +1,11 @@
-<!DOCTYPE html>
-
-<html>
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="styleT.css" />
-    <title>Nom du site</title>
-</head>
-<body>
-  <div class="header">
-      <div class="button" id="accueil">Accueil</div>
-      <div class="button" id="connexion">Connexion</div>
-      <div class="button" id="recommandes">Recommandés</div>
-      <div class="button" id="new">Nouveautés</div>
-      <div class="button" id="ma_page">Ma</div>
-
-    <div id="devise">On pourrait mettre une devise ici</div>
-    <div id ="rech">
-    <form action="recherche.php" method="Post">
-    <input type="text" name="nom" size="10" />
-    <input type="submit" value="Ok">
-    </form>
-    </div>
-    <div class="header" id="logo"></div>
-    </div>
-
-  </div>
+<?php
+session_start();
+include 'api.php';
+afficher_menu();
+?>
   <div class = "corps">
 <?php
 $asked = $_POST['nom'];
-$servername = "10.0.3.100";
-$username = "equipe";
-$password = "coucou";
-$dbname = "cinema";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
  if ($conn->connect_error) {
